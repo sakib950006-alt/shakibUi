dd6cc8e2359a376c1df71d07cb8584b04a252cb9
Shaqy2024 <sakib950006@gmail.com>
2025-12-17 13:10:29 +0530

    init

---
+++ config/site.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+    name: "Codesnippetui",
+    url: "https://Codesnippetui.com",
+    ogImage: "#",
+    description:
+        "Copy, Paste, Customize. A collection of modern, interactive, customizable UI components. Quick and easy to use.",
+    links: {
+        twitter: "#",
+        github: "#",
+    },
+};
+
+export type SiteConfig = typeof siteConfig;
+
+export const META_THEME_COLORS = {
+    light: "#fafafa",
+    dark: "#09090b",
+};
